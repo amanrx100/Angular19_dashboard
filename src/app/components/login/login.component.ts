9fd9e0f6b542ba4e6d6a97e737a8bcc4f8272837
amanrx100 <amankumawat95.6@gmail.com>
2025-07-01 21:12:13 +0530

    Login API added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+        username: ['', Validators.required],
+        password: ['', Validators.required]
+    })
+  }
+   onSubmit(){
+      const formData = this.loginForm.value;
+      this.http.post('https://dummyjson.com/auth/login', formData).subscribe({
+        next: (res) => {
+          this.router.navigate(['/dashboard']);
+        }, error: (err) => {
+          console.log(err);
+          
+        }
+      })
+   }
 }
